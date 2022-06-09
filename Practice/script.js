56cdb463c9acf945700a9dc8afb7db1f969ace7b
SwapnilParate <101567137+SwapnilParate@users.noreply.github.com>
2022-06-09 21:19:45 +0530

    Add files via upload

---
+++ Practice/script.js
@@ -0,0 +1,28 @@
+let intvar=10;
+let strvar="Swapnil";
+let arrvar = [23,34,"Hi","Swapnil",45,true];
+let boovar=true;
+let objvar={"Name":"Swapnil","Age":23,"City":"Nagpur"};
+
+
+var num = prompt("Enter number");
+console.log(num);
+var num2 = prompt("Enter 2nd number");
+console.log(num2);
+
+var sum = (+num) + (+num2);
+console.log(sum);
+
+/*let j=0;
+for (j=0; j < arrvar.length; j++)
+{
+    console.log(arrvar[j]);
+}*/
+
+
+
+//console.log(intvar);  console.log(typeof(intvar));
+//console.log(strvar);  console.log(typeof(strvar));
+//console.log(arrvar);  console.log(typeof(arrvar));
+//console.log(boovar);  console.log(typeof(boovar));
+//console.log(objvar);  console.log(typeof(objvar));
